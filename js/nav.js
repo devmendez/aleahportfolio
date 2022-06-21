7a57cdb3ef0f007712ace2d856b5243e3b2b46eb
Devina Mendez <devmendez@gmail.com>
2022-06-20 22:50:55 -0500

    index styling

--- js/nav.js
+++ js/nav.js
@@ -3,15 +3,19 @@ const createNav = () => {
 let nav = document.querySelector('nav');
 
 nav.innerHTML = `
-<div class="navbar-container">
 <div class="navbar">
-<img id="logo" src="logo" alt="personal logo">
-<ul>
-  <li><a href="/index.html">About Me</li>
-  <li><a href="/projects.html">Projects</li>
-  <li><a href="/resume.html">Resume</li>
-</ul>
-</div>
+  <div class="navbar-container">
+    <div class="logo-container">
+      <img id="logo" src="logo" alt="personal logo">
+    </div>
+    <div class="nav-links-container">
+      <ul class="nav-links">
+        <li><a href="/index.html">About Me</li>
+        <li><a href="/projects.html">Projects</li>
+        <li><a href="/resume.html">Resume</li>
+      </ul>
+    </div>
+  </div>
 </div>
 
 `;
