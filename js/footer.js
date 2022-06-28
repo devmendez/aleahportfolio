758b7d83c4524717f965256e0721e45aa1615be5
Devina Mendez <devmendez@gmail.com>
2022-06-27 19:21:20 -0500

    some more changes

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,7 @@ footer.innerHTML = `
 <div class="footer">
   <div class="footer-container">     
         <p><i class="fas fa-envelope" id="envelope"></i><a href="mailto:aleahragan06@gmail.com" target="_blank">aleahragan06@gmail.com</a></p>
-        <p><i class="fa-brands fa-linkedin" id="linked"></i><a href="www.linkedin.com/in/aleah-ragan" target="_blank">www.linkedin.com/in/aleah-ragan</a></p>
+        <p><i class="fa-brands fa-linkedin" id="linked"></i><a href="https://www.linkedin.com/in/aleah-ragan/" target="_blank">www.linkedin.com/in/aleah-ragan</a></p>
   </div>
 </div>
 `;
