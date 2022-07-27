cc136d0850ca3a51703317861cccccb6dd65c085
Devina Mendez <devmendez@gmail.com>
2022-07-26 19:57:35 -0500

    more edits

--- js/footer.js
+++ js/footer.js
@@ -4,9 +4,9 @@ let footer = document.querySelector('footer');
 
 footer.innerHTML = `
 <div class="footer">
-  <div class="footer-container">     
-       <a href="mailto:aleahragan06@gmail.com" target="_blank"> <i class="fas fa-envelope" id="envelope"><p class="footer-link">aleahragan06@gmail.com</p></i></a>
-       <a href="https://www.linkedin.com/in/aleah-ragan/" target="_blank"><i class="fab fa-brands fa-linkedin" id="linked"><p class="footer-link">www.linkedin.com/in/aleah-ragan</p></i></a>
+  <iv class="footer-container">    
+       <a href="mailto:aleahragan06@gmail.com" target="_blank"> <i class="fas fa-envelope" id="envelope"></i></a>
+       <a href="https://www.linkedin.com/in/aleah-ragan/" target="_blank"><i class="fab fa-brands fa-linkedin" id="linked"></i></a>
   </div>
 </div>
 `;
